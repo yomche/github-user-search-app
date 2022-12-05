af1b1090eb0e61d3d858047a32aba93b7e87e8a2
Ekaterina Dorozhkina <katerina.smirnova98@yandex.ru>
2022-12-05 15:09:03 +0300

    project complete

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "yomche",
+  avatar: "https://avatars.githubusercontent.com/u/31934798?v=4",
+  name: "Ekaterina",
+  company: "",
+  blog: "",
+  location: "",
+  bio: "",
+  twitter: "",
+  repos: 8,
+  followers: 3,
+  following: 2,
+  created: "2017-09-13T18:14:26Z",
+};
